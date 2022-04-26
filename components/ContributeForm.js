d6aef2138cd6645522e707a323ec0cb2c06d90f7
DBowen33 <42016383+DBowen33@users.noreply.github.com>
2022-04-25 19:11:06 -0500

    added requests flow

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
 import { Form, Input, Message, Button } from 'semantic-ui-react'
 import Campaign from '../ethereum/campaign'
+import web3 from '../ethereum/web3';
+import { Router } from '../routes'
 
 class ContributeForm extends Component {
     state = {
         value: '',
-        loading: false
+        loading: false,
+        errorMessage: ''
     };
 
-    onSubmit = (event) => {
+    onSubmit = async (event) => {
         event.preventDefault();
         const campaign = Campaign(this.props.address);
+
+        this.setState({ loading: true, errorMessage: '' });
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            })
+
+            Router.replaceRoute(`/campaigns/${this.props.address}`);
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
+
+        this.setState({ loading: false, value: ''})
 
     }
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <label>Amount to Contribute</label>
                 <Form.Field>
                     <Input
@@ -25,7 +44,9 @@ class ContributeForm extends Component {
                         value={this.state.value}
                         onChange={event => this.setState({ value: event.target.value })}/>
                 </Form.Field>
-                <Button primary>Contribute!</Button>
+
+                <Message error header="Oops!" content={this.state.errorMessage} />
+                <Button loading={this.state.loading} primary>Contribute!</Button>
             </Form>
         );
     }
